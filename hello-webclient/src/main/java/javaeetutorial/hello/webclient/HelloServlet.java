f6576486a68fdacffd5bfe523b5cb0174feac395
Bernardo Erlacher Roubach <beroubach@fitec.org.br>
2017-06-23 14:48:22 -0300

    Initial load

---
+++ hello-webclient/src/main/java/javaeetutorial/hello/webclient/HelloServlet.java
@@ -0,0 +1,50 @@
+package javaeetutorial.hello.webclient;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import javaeetutorial.helloservice.endpoint.HelloService;
+
+@WebServlet(name = "HelloServlet", urlPatterns = { "/HelloServlet" })
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+
+			out.println("<html lang=\"en\">");
+			out.println("<head>");
+			out.println("<title>Servlet HelloServlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
+			out.println("<p>" + sayHello("world") + "</p>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+	}
+
+	// doGet and doPost methods, which call processRequest, and
+	// getServletInfo method
+	private String sayHello(java.lang.String arg0) throws MalformedURLException {
+		URL wsdlUrl = new URL("http://localhost:8080/helloservice-war-7.0.6/HelloService?WSDL");
+		QName qName = new QName("http://localhost:8080/helloservice-war-7.0.6/HelloService", "HelloService");
+		Service service = Service.create(wsdlUrl, qName);
+		HelloService helloService = service.getPort(HelloService.class);
+
+		javaeetutorial.helloservice.endpoint.Hello port = helloService.getHelloPort();
+		return port.sayHello(arg0);
+	}
+}
